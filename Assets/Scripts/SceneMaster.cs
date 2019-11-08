779ab3d76e5d44dfbbf0024fed42d60f794945a9
AlexLiuTafe <gloria_domini@hotmail.com>
2019-11-08 12:13:12 +1100

    Update Sword + Change Scene

---
+++ Assets/Scripts/SceneMaster.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneMaster : MonoBehaviour
+{
+	private Scene currentScene;
+
+	private void Start()
+	{
+		currentScene = SceneManager.GetActiveScene();
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+        if(Input.anyKeyDown)
+		{
+			if(currentScene.buildIndex.Equals(3))
+			{
+				SceneManager.LoadScene(0);
+			}
+			else
+			{
+				NextScene();
+			}
+			
+		}
+    }
+
+	private void NextScene()
+	{
+		SceneManager.LoadScene(currentScene.buildIndex + 1);
+	}
+}
